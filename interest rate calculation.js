6d1f875ea8ed5117cdc40bff88d17e12e3ca0fb4
Spencer Saras <154802481+SpencerfoSaras@users.noreply.github.com>
2023-12-25 12:19:03 +0200

    Create interest rate calculation.js

---
+++ interest rate calculation.js
@@ -0,0 +1,22 @@
+{
+  "NodeVersion": "0.0.1",
+  "ID": "testTradeId3",
+  "CoinInputs": null,
+  "ItemInputs": [
+    {
+      "CookbookID": "cb1",
+      "ItemInput": {
+        "ID": "",
+        "Doubles": [
+          {
+
+                  "TransferFee": {
+          "MinValue": "0",
+          "MaxValue": "0"
+        }
+      }
+    }
+  ],
+
+          "Sender": "testId",
+  "FulFiller": "testId2",
